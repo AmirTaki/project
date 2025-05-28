a0d0f95f09e93a98734d830d95944e192bd12385
amir <amirtaki623@gmail.com>
2025-05-28 03:42:03 +0330

    style @media header down

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 let iconQuery = document.querySelector('.icon-query')
-
+let iconSearchHeader = document.querySelector(".header-search  .bi-search") 
 // resize
 const resize_window = () => {
     if (window.innerWidth < 850){
         iconQuery.style.display = ""
+        iconSearchHeader.style.display = ""
     }
     else {
         iconQuery.style.display = "none"
+        iconSearchHeader.style.display = "none"
     }
 }
 // resize

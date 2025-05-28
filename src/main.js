f004901d8a73ab446fbec9ad957b96a8563549a3
amir <amirtaki623@gmail.com>
2025-05-29 01:54:09 +0330

    edit

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ const resize_window = () => {
     if (window.innerWidth < 850){
         iconQuery.style.display = ""
         iconSearchHeart.className = "bi bi-search"
-        console.log(iconSearchHeart.classList)
     }
     else {
         iconQuery.style.display = "none"

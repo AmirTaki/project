6ca96b9aa4b3ba67622e63e16006386de26384c2
amir <amirtaki623@gmail.com>
2025-05-29 03:38:23 +0330

    js : list-main-item-title

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 let iconQuery = document.querySelector('.icon-query')
 let iconSearchHeart = document.getElementById("icon-search-heart")
+let listUl = document.querySelectorAll(".list-main-item-title")
+let listLi = document.querySelectorAll(".list-main-item-title div")
 
+
+const hiddenListLi = ()=> {
+    for (let i = 0; i < listLi.length; i++){
+        listLi[i].style.display = 'none'
+    }
+}
+const showListLi = ()=> {
+      for (let i = 0; i < listLi.length; i++){
+        listLi[i].style.display = ''
+    }
+}
 // resize
 const resize_window = () => {
     if (window.innerWidth < 850){
         iconQuery.style.display = ""
         iconSearchHeart.className = "bi bi-search"
+        hiddenListLi ()
     }
     else {
         iconQuery.style.display = "none"
         iconSearchHeart.classList = 'bi bi-heart'
+        showListLi()
     }
 }
 // resize
@@ -19,3 +34,10 @@ window.addEventListener("resize",(e)=> {
 resize_window()
 
 
+
+for (let i = 0; i< listUl.length; i++ ){
+    listUl[i].addEventListener("click", (event)=>{
+       console.log("ok")
+        listLi[i].style.display = ""
+    })
+}

cd79acb78052a2c8d6b60b0b04389dbf5a42f8ec
amir <amirtaki623@gmail.com>
2025-05-30 21:46:16 +0330

    checkName value === 0

--- src/script/sign.js
+++ src/script/sign.js
@@ -3,3 +3,23 @@ let name = document.getElementById("name")
 let email = document.getElementById('email')
 let password = document.getElementById('password')
 let repeatPassword = document.getElementById("repeat-password")
+let button = document.getElementById("button")
+let span = document.querySelectorAll("span")
+let input = document.querySelectorAll("input")
+
+
+console.log(span)
+const checkName = () => {
+   
+    if (name.value.length == 0){
+        span[0].innerText = "name is required"
+        span[0].style.color = 'red'
+        input[0].style.borderColor = "red"
+    }
+}
+
+button.addEventListener('click', (e)=>{
+    e.preventDefault()
+    checkName()
+})
+

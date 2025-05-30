2cbcd1c00287efa3068344aa1a3613558c8f07ce
amir <amirtaki623@gmail.com>
2025-05-30 22:12:33 +0330

    js : check name for signup

--- src/script/sign.js
+++ src/script/sign.js
@@ -6,26 +6,34 @@ let repeatPassword = document.getElementById("repeat-password")
 let button = document.getElementById("button")
 let span = document.querySelectorAll("span")
 let input = document.querySelectorAll("input")
+let icon = document.querySelectorAll("form i")
 
 
-console.log(span)
 const checkName = () => {
    
     if (name.value.length == 0){
         span[0].innerText = "Name is required"
         span[0].style.color = 'red'
         input[0].style.borderColor = "red"
-        // <i class="bi bi-x-circle"></i>
+        icon[0].className = "bi bi-x-circle"
+        icon[0].style.color = "red"
+        return false
     }
     else if(!name.value.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
         span[0].innerText = "Write full Name"
         span[0].style.color = 'red'
         input[0].style.borderColor = "red"
+        icon[0].className = "bi bi-x-circle"
+        icon[0].style.color = "red"
+        return false
     }
     else {
-        span[0].innerText = "Write full Name"
+        span[0].innerText = ""
         span[0].style.color = 'green'
-        input[0].style.borderColor = "green"
+        input[0].style.borderColor = "black"
+        icon[0].className = "bi bi-check-circle"
+        icon[0].style.color = "green"
+        return true
     }
 }
 

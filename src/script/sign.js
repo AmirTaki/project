87a73b1ed605a4e3675e246924a595bb64969e61
amir <amirtaki623@gmail.com>
2025-05-30 21:29:29 +0330

    sign .js

---
+++ src/script/sign.js
@@ -0,0 +1,5 @@
+let form = document.querySelector('form')
+let name = document.getElementById("name")
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let repeatPassword = document.getElementById("repeat-password")

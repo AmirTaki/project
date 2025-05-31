0257c7475d9ae92aa37676851c53b3cc7ef5c663
amir <amirtaki623@gmail.com>
2025-05-31 19:49:37 +0330

    styles css panale admin

--- panel/index.php
+++ panel/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel admin adidas</title>
+    <link rel="stylesheet" href="../src/styles/panelAdimin.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
     

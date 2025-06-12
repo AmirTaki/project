a5d544630d6fee0436d7728e666e625ffb32482b
amir <amirtaki623@gmail.com>
2025-06-12 22:01:07 +0330

    add .iconssearch hedaers styles at project

--- src/script/main.js
+++ src/script/main.js
@@ -10,30 +10,23 @@ searchIconInput.addEventListener("focusout", (e)=> {
     iconSearch.style.opacity = 1
 })
 
-iconSearch.addEventListener('click', (e)=>{
-  
-    if(searchIconInput.style.opacity == 0){
 
-        searchIconInput.style.opacity = 1
-        searchIconInput.style.animation  = "searchInput .5s linear forwards " 
-          
-    } 
-    else {
-        searchIconInput.style.animation  = "searchInputReverse .5s linear forwards "
-        setTimeout(() => {
-            searchIconInput.style.opacity = 0
-        }, 500);
-//    
- 
-    }
-
-})
 const HideFlexInputSearchHeader = () =>{
     searchIconInput.style.animation  = "searchInputReverse .5s linear forwards "
     setTimeout(() => {
         searchIconInput.style.opacity = 0
     }, 500);
 }
+
+iconSearch.addEventListener('click', (e)=>{
+    if(searchIconInput.style.opacity == 0){
+        searchIconInput.style.opacity = 1
+        searchIconInput.style.animation  = "searchInput .5s linear forwards " 
+    } 
+    else {
+        HideFlexInputSearchHeader ();
+    }
+})
 
 window.addEventListener("resize", (e)=>{
     HideFlexInputSearchHeader()

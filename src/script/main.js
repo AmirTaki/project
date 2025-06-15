fdf6050003d4c15b723b71f962885626651ffb34
amir <amirtaki623@gmail.com>
2025-06-15 23:22:13 +0330

    // MOVE ANIMATION CONATAINER PAGE SIDEBAR js

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,8 @@
 let iconMenu = document.querySelector('.iconMenu')
 let menuContainer = document.querySelector(".menuContainer")
 let crossSideBar = document.querySelector(".crossSideBar")
+let listTitle = document.querySelectorAll('.listTitle')
+let pageContainer = document.querySelectorAll('.pageContainer')
 // MOVE ANIMATION SIDEBAR
 iconMenu.addEventListener("click", (e)=>{
     menuContainer.style.display = "flex"
@@ -10,6 +12,15 @@ iconMenu.addEventListener("click", (e)=>{
 crossSideBar.addEventListener("click", (e)=>{
     menuContainer.style.animation = "moveReverseSidebar .5s linear forwards"
 })
+// MOVE ANIMATION CONATAINER PAGE SIDEBAR
+for(let i = 0; i < listTitle.length; i++){
+    listTitle[i].addEventListener("click", (e)=>{
+        if(window.innerWidth < 750){
+            pageContainer[i].style.display = 'flex'
+            pageContainer[i].style.animation = "movePageContainer .5s linear forwards"
+        }
+    })
+}
 // WINDOW ADD EVENET RESIZE 
 window.addEventListener('resize', (e)=>{
     if(window.innerWidth < 750){

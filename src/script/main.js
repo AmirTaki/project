77d7b94aa2c58f818d4e6eb5d64ab6da12cfbaa7
amir <amirtaki623@gmail.com>
2025-06-15 22:41:37 +0330

    MOVE REVERSE  ANIMATION SIDEBAR js at project

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,12 @@
 let iconMenu = document.querySelector('.iconMenu')
 let menuContainer = document.querySelector(".menuContainer")
+let crossSideBar = document.querySelector(".crossSideBar")
 // MOVE ANIMATION SIDEBAR
 iconMenu.addEventListener("click", (e)=>{
     menuContainer.style.display = "flex"
     menuContainer.style.animation = "moveSidebar .5s linear forwards"
 })
 // REVERSE MOVE ANIMATIO SIDEBAR
+crossSideBar.addEventListener("click", (e)=>{
+    menuContainer.style.animation = "moveReverseSidebar .5s linear forwards"
+})

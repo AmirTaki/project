8d9ecd426d633c7c8b05a1fc86178a2bbadae05f
amir <amirtaki623@gmail.com>
2025-06-18 03:10:48 +0330

    container scroll wheel at project

--- src/script/main.js
+++ src/script/main.js
@@ -2,12 +2,16 @@ let iconLeftScrollImg = document.querySelector(".iconLeftScrollImg")
 let iconRigthtScrollImg = document.querySelector(".iconRigthtScrollImg")
 let containerScroll = document.querySelector(".containerScroll")
 
+
+containerScroll.addEventListener("wheel",(e)=>{
+    e.preventDefault()
+    containerScroll.scrollLeft += e.deltaY;
+})
 iconLeftScrollImg.addEventListener("click",(e)=>{
     containerScroll.style.scrollBehavior = "smooth";
     containerScroll.scrollLeft -= 300
 })
 iconRigthtScrollImg.addEventListener("click",(e)=>{
-    console.log("ok")
     containerScroll.style.scrollBehavior = "smooth";
     containerScroll.scrollLeft += 300
 })

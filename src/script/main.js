fc862e8d70b40aa44f54e4a5ca30294f1f783266
amir <amirtaki623@gmail.com>
2025-06-18 15:21:59 +0330

    banner & sliderBanner at project

--- src/script/main.js
+++ src/script/main.js
@@ -2,15 +2,17 @@ let iconLeftScrollImg = document.querySelector(".iconLeftScrollImg")
 let iconRigthtScrollImg = document.querySelector(".iconRigthtScrollImg")
 let containerScroll = document.querySelector(".containerScroll")
 
-
+// CONTAINER SCROLL IMAGE WHELL
 containerScroll.addEventListener("wheel",(e)=>{
     e.preventDefault()
     containerScroll.scrollLeft += e.deltaY;
 })
+// CONTAINER SCROLL IMAGE CLICK LEFT
 iconLeftScrollImg.addEventListener("click",(e)=>{
     containerScroll.style.scrollBehavior = "smooth";
     containerScroll.scrollLeft -= 300
 })
+// CONTAINER SCROLL IMAGE CLICK RIGHT
 iconRigthtScrollImg.addEventListener("click",(e)=>{
     containerScroll.style.scrollBehavior = "smooth";
     containerScroll.scrollLeft += 300
